c7da1f5550f26fefb27141a8b5d1b3f4c6789918
vldmirr <tsutsura18@gmail.com>
2025-10-16 19:07:55 +0300

    добавли логгер

---
+++ internal/logger/logger.go
@@ -0,0 +1,23 @@
+package logger
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	InfoLogger  *log.Logger
+	WarnLogger  *log.Logger
+	ErrorLogger *log.Logger
+)
+
+func NewLogger(){
+	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	InfoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	WarnLogger = log.New(file, "WARN: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
